ffe1f31a8a21521ab296c933cfdc39c4d46c66da
Bopzor <violaine.ballandin@gmail.com>
2021-05-27 09:46:28 +0200

    update polybar.sh

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+# Launch top
+echo "---" | tee -a /tmp/polybar.log
+polybar top 2>&1 | tee -a /tmp/polybar.log & disown
 
-# Launch polybar
-polybar top &
+echo "Bars launched..."
