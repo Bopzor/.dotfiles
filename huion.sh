086750a099eab08ba85aafe7022f7744b3778c95
Bopzor <violaine.ballandin@gmail.com>
2021-12-19 12:24:14 +0100

    add toggle keyboad layout

---
+++ huion.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xsetwacom --set 'HUION Huion Tablet Pad pad' Button 1 "key +ctrl +z -z -ctrl"
+xsetwacom --set 'HUION Huion Tablet Pad pad' Button 2 "key shift e"
+xsetwacom --set 'HUION Huion Tablet Pad pad' Button 3 "key b"
+xsetwacom --set 'HUION Huion Tablet Pad pad' Button 8 "key shift +"
+xsetwacom --set 'HUION Huion Tablet Pad pad' Button 9 "key -"
+xsetwacom --set 'HUION Huion Tablet Pad pad' Button 10 "key ]"
+xsetwacom --set 'HUION Huion Tablet Pad pad' Button 11 "key ["
+xsetwacom --set 'HUION Huion Tablet Pad pad' Button 12 "key p"
