6d011007b9b6a5f6b39fcee065c525775a3f1699
Bopzor <violaine.ballandin@gmail.com>
2022-01-19 19:46:37 +0100

    setup stylus of graphic tablet

--- huion/huion.sh
+++ huion/huion.sh
@@ -8,3 +8,6 @@ xsetwacom --set 'HUION Huion Tablet Pad pad' Button 9 "key -"
 xsetwacom --set 'HUION Huion Tablet Pad pad' Button 10 "key ]"
 xsetwacom --set 'HUION Huion Tablet Pad pad' Button 11 "key ["
 xsetwacom --set 'HUION Huion Tablet Pad pad' Button 12 "key p"
+
+# fix stylus boundarybox to primary screen only
+xinput map-to-output "HUION Huion Tablet stylus" "eDP-1"
