be4ec8d82befa8d2f114d10277a1be8e61a4713e
Bopzor <26028196+Bopzor@users.noreply.github.com>
2025-03-10 14:37:51 +0100

    update

--- sway/scripts/move-scratchpad.sh
+++ sway/scripts/move-scratchpad.sh
@@ -9,15 +9,19 @@ fi
 outputs=$(swaymsg -r -t get_outputs | jq '.[] | select(.focused)')
 
 mode=$(echo "$outputs" | jq '.current_mode')
-scale=$(echo "$output" | jq '.scale')
+scale=$(echo "$outputs" | jq '.scale')
 
-screen_width=$(echo "$mode" | jq '.width') / $scale
-screen_height=$(echo "$mode" | jq '.height') / $scale
+screen_width=$(echo "$mode" | jq '.width')
+screen_height=$(echo "$mode" | jq '.height')
+
+# use awk rounding capabilities to calculate floating numbers
+scaled_screen_height=$(echo "$screen_height $scale" | awk '{print $1 / $2}')
+scaled_screen_width=$(echo "$screen_width $scale" | awk '{print $1 / $2}')
 
 term_width='720'
-term_height="$(($screen_height - 38))"
+term_height="$(($scaled_screen_height - 38))"
 
-term_x="$(($screen_width - $term_width))"
+term_x="$(($scaled_screen_width - $term_width))"
 term_y='0'
 
 swaymsg "resize set width $term_width height $term_height"
